615de446ef3ffbb571fc281c11628fdfc4a671aa
Camilo <camilo.perezv@udea.edu.co>
2017-04-01 09:39:15 -0500

    peticion http

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,10 +1,30 @@
 <template lang="html">
-    <p>Users View</p>
+  <div>
+      <h2>Users View</h2>
+      <ul>
+        <li v-for="user in users">
+          {{ user.name }}
+        </li>
+      </ul>
+  </div>
 </template>
 
 <script type="text/javascript">
+  import axios from 'axios';
   export default{
-
+    data(){
+      return{
+        users:[]
+      }
+    },
+    mounted(){
+      axios.get('https://jsonplaceholder.typicode.com/users')
+        .then(success=>{
+          this.users = success.data;
+        },error=>{
+          console.log('oppppps',error);
+        });
+    }
   }
 </script>
 <style type="text/css">
