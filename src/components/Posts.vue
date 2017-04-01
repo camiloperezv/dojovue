e3cd52a5f9be888f72ec338e6c6fd638a0f1a62c
Camilo <camilo.perezv@udea.edu.co>
2017-04-01 09:20:19 -0500

    ruta de posts

---
+++ src/components/Posts.vue
@@ -0,0 +1,12 @@
+<template lang="html">
+    <p>Los POsts</p>
+</template>
+
+<script type="text/javascript">
+  export default{
+
+  }
+</script>
+<style type="text/css">
+  
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import Users from '@/components/Users'
-
+import Posts from '@/components/Posts'
 Vue.use(Router)
 
 export default new Router({
@@ -16,6 +16,11 @@ export default new Router({
       path: '/users',
       name: 'Users',
       component: Users
+    },
+    {
+      path: '/posts',
+      name: 'Posts',
+      component: Posts
     }
   ]
 })
